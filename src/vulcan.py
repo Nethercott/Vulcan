e4d4e9af211ff719a03c40a0afc482d068902401
Christopher Nethercott <ccnethercott@gmail.com>
2018-04-30 22:06:38 +0100

    Writing down my thoughts

--- src/vulcan.py
+++ src/vulcan.py
@@ -6,6 +6,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
+sheet = client.open("Vulcan").sheet1
+row_count = sheet.row_count
 # DS18B20
 probes = DS18B20()
 probe_count = probes.device_count()
@@ -16,7 +18,12 @@ So the old temperature is 18C and the new temperature is 21C, offset is 2, this 
 def rangeCheck(old_number, offset, new_number): # Thanks @Eladkay, for this
     return not new_number <= (old_number + offset) and new_number > (old_number - offset)
 
+"""
+Get the last temperatures posting, compare to the current temperatures. If they exceed offsets, send an update to the google sheet
+"""
 def main():
+
+
 
 if __name__ == '__main__':
     main()
